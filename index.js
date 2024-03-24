ffaabbb85cb1473875b1119bcf2208735e0d9adf
RodgersTK <rodgerstkm@gmail.com>
2024-03-24 05:48:08 +0300

    updating the Scuber

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your solution in this file!
+var customerName = 'bob';
+function upperCaseCustomerName() {
+    window.customerName = window.customerName.toUpperCase();
+  }
+  function setBestCustomer() {
+    window.bestCustomer = 'not bob';
+  }
+  var bestCustomer = 'maybe bob';
+  function overwriteBestCustomer(newBestCustomer) {
+    bestCustomer = newBestCustomer;
+  }
+  const leastFavoriteCustomer = 'someone';
+  function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'new person';
+  }
